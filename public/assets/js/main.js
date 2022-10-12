26c695dc679244bf70ffd7a4d6d4224ecdd4f4cf
Administrator <team@project.com>
2022-10-12 14:11:05 +0200

    test Connection

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,7 @@
 let xhr = new XMLHttpRequest(),
-root = document.querySelector("[path-ref=root]")
-xhr.open("POST",`${root}user\add`)
+root = document.querySelector("[path-ref=root]").getAttribute("url")
+console.log(root)
+xhr.open("POST",`${root}user/add`)
 xhr.onload = function(){
     console.log(xhr.responseText)
 }
