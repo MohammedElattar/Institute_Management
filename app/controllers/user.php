7adb7408d1de90d232d60f0b542cb66a0624eed8
Administrator <team@project.com>
2022-10-12 15:53:59 +0200

    user

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -3,5 +3,8 @@ class User_Controller extends Controller
 {
     public function add($param)
     {
+        $_POST = json_decode(file_get_contents('php://input'));
+        echo "This is User Add Section , Your Posted Data are \n\n";
+        print_r($_POST);
     }
 }
