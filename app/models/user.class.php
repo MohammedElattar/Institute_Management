9e76be7e685cf798d2874873270f10bda8645516
Administrator <team@project.com>
2022-10-16 00:08:34 +0200

    user Authentication

--- app/models/user.class.php
+++ app/models/user.class.php
@@ -3,9 +3,29 @@ class User
 {
     public function login($POST)
     {
-        $username = @$POST['username'] or null;
-        $password = @$POST['password'] or null;
-        $role = @$POST['role'] or null;
+        $email = @trim(htmlentities($POST['email'])) or null;
+        $password = @trim(htmlentities($POST['password'])) or null;
+        $role = @trim(htmlentities($POST['role'])) or null;
+        $res = [];
+        $roles = ['student' => 'students', 'manager' => 'admin', 'teacher' => 'teachers'];
+        if (!$email) $res['empty-email'] = '1';
+        else if (filter_var($email, FILTER_VALIDATE_EMAIL)) $res['not-valid-email'] = '1';
+        else if (!$password) $res['empty-pass'] = '1';
+        else if (!in_array($role, ['student', 'manager', 'teacher'])) $res['role-not-exists'] = '1';
+        else {
+            $db = DB::get_instance();
+            $user = $db->read("SELECT id FROM $roles[$role] WHERE email=? AND password=?", [$email, sha1($password), true]);
+            if (!$user[1]) $res['user-not-exists'] = '1';
+            else {
+                $user = $user[0][0];
+                $_SESSION['logged'] = '1';
+                $_SESSION['data']['id'] = $user['id'];
+                $_SESSION['data']['email'] = $email;
+                $_SESSION['data']['role'] = $role;
+                $res['success'] = '1';
+            }
+        }
+        return $res;
     }
     public function signup()
     {
