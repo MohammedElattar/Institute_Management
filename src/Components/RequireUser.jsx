ee9cc99c305021efaf0b1eab4a6b66da64d30d96
Administrator <team@project.com>
2022-10-18 23:12:29 +0200

    returning data

--- src/Components/RequireUser.jsx
+++ src/Components/RequireUser.jsx
@@ -5,9 +5,9 @@ import { useGlobalContext } from "../context";
 const RequireUser = ({ children }) => {
   const { user } = useGlobalContext();
 
-  if (!user) {
-    return <Navigate to="/login" />;
-  }
+  // if (!user) {
+  //   return <Navigate to="/login" />;
+  // }
 
   return children;
 };
